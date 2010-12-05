meltingice/CamanJS

commit 0bd53bcd6e970f80ab307f487d240cd4cf5d1b7a

push registration of .onload handler outwards to avoid race condition (wrt DOMContentLoaded on the doc) + avoid posting shared object structure from worker (cannot be relied upon just yet.)

caman.js
@@ -88,20 +88,13 @@
 
           img.src = options.src; 
 
+          img.onload = function() {
+             imageReady.call(that);
+          };
           if ( !Caman.ready ) {
             document.addEventListener("DOMContentLoaded", function() {
               Caman.ready = true;
-              
-              img.onload = function() {
-                  imageReady.call(that);  
-              };
-            }, false);        
-          } else {
-
-            img.onload = function() {
-              imageReady.call(that);  
-            };
-          }
+	  }
         } else {
           // Handle Caman('#index')
           return Caman.store[options];
@@ -665,7 +658,7 @@
                   "processFn" : self.queue[next].process.toString(),
                   "processFnName" : self.queue[next].process.name,
                   "adjust": self.queue[next].adjust
-                });                  
+                });
               }
                             
               self.inProcess = false;
@@ -692,7 +685,7 @@
           "processFn" : processFn.toString(),
           "processFnName" : processFn.name,
           "adjust": adjust
-        });      
+        });
       }
         
 
@@ -752,8 +745,7 @@ onmessage = function( event ) {
   // TODO: add rest of data object
   postMessage({
     "processFnName": data.processFnName, 
-    "pixelData" : data.pixelData,
-    "data" : data
+    "pixelData" : data.pixelData
   });
 };
 // WorkerGlobalScope //

commit ad1830fdd95d1d7ff01cf6e3fb14d0addefca78d

Fixes bug introduced in 0bd53bcd6e970f80ab30

caman.js
@@ -91,10 +91,14 @@
           img.onload = function() {
              imageReady.call(that);
           };
+          
           if ( !Caman.ready ) {
             document.addEventListener("DOMContentLoaded", function() {
               Caman.ready = true;
-	  }
+            }, false);          
+          }
+          
+          
         } else {
           // Handle Caman('#index')
           return Caman.store[options];