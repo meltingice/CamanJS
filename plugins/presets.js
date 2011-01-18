23475f365ab2d16f0bd77e105350b4e18547297f
meltingice <meltingice8917@gmail.com>
2011-01-18 14:54:53 -0500

    Updated cross-processing preset a bit... looks better now

--- plugins/presets.js
+++ plugins/presets.js
@@ -2,22 +2,24 @@
 
 Caman.manip.crossProcess = function () {
   return this
-	   .curves('r', [0, 0], [10, 100], [200, 160], [220, 255])
-	   .curves('b', [0, 50], [75, 86], [175, 160], [255, 220])
-	   .curves('g', [0, 0], [100, 100], [150, 195], [255, 255]);
+    .curves('r', [0, 0], [130, 50], [125, 205], [255, 255])
+    .curves('b', [0, 50], [20, 120], [235, 135], [255, 255])
+    .curves('g', [0, 0], [130, 50], [125, 205], [255, 255])
+    .brightness(3)
+    .contrast(5);
 };
 
 Caman.manip.vintage = function () {
   return this
-      .saturation(40)
-      .contrast(5)
-      .curves('r', [0, 0], [125, 100], [220, 230], [220, 255])
-      .curves('g', [0, 0], [120, 120], [128, 190], [255, 255])
-      .curves('b', [0, 30], [0, 30], [255, 205], [255, 205])
-      .colorize('#ff56aa', 10)
-      .sepia(50)
-      .saturation(-20)
-      .vignette(120, 70);
+    .saturation(40)
+    .contrast(5)
+    .curves('r', [0, 0], [125, 100], [220, 230], [220, 255])
+    .curves('g', [0, 0], [120, 120], [128, 190], [255, 255])
+    .curves('b', [0, 30], [0, 30], [255, 205], [255, 205])
+    .colorize('#ff56aa', 10)
+    .sepia(50)
+    .saturation(-20)
+    .vignette(120, 70);
 };
 
 }(Caman));
