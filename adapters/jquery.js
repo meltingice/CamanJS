16a8afb76b94f3f80b9379766b8f50d54bf83f18
meltingice <meltingice8917@gmail.com>
2011-02-25 16:41:05 -0500

    Some source formatting fixes

--- adapters/jquery.js
+++ adapters/jquery.js
@@ -2,9 +2,9 @@
  * jQuery plugin adapter for CamanJS
  */
 if (window.jQuery) {
-	window.jQuery.fn.caman = function (callback) {
-		return this.each(function () {
-			Caman(this, callback);
-		});
-	};
+  window.jQuery.fn.caman = function (callback) {
+    return this.each(function () {
+      Caman(this, callback);
+    });
+  };
 }
