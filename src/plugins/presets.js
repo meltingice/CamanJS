a77b747ab24e58271986fe88fe776f4a1e059c27
meltingice <meltingice8917@gmail.com>
2011-02-27 23:06:36 -0500

    New preset filter: hazyDays

--- src/plugins/presets.js
+++ src/plugins/presets.js
@@ -173,4 +173,46 @@ Caman.manip.glowingSun = function () {
   return this.vignette(250, 20);
 };
 
+Caman.manip.hazyDays = function () {
+  this.gamma(1.2);
+    
+  this.newLayer(function () {
+    this.setBlendingMode('overlay');
+    this.opacity(60);
+    this.copyParent();
+    
+    this.filter.channels({red: 5});
+    this.filter.heavyRadialBlur();
+  });
+  
+  this.newLayer(function () {
+   this.setBlendingMode('addition');
+   this.opacity(40);
+   this.fillColor('#6899ba');
+  });
+  
+  this.newLayer(function () {
+    this.setBlendingMode('multiply');
+    this.opacity(35);
+    this.copyParent();
+    
+    this.filter.brightness(40);
+    this.filter.vibrance(40);
+    this.filter.exposure(30);
+    this.filter.contrast(15);
+    
+    this.filter.curves('r', [0, 40], [128, 128], [128, 128], [255, 215]);
+    this.filter.curves('g', [0, 40], [128, 128], [128, 128], [255, 215]);
+    this.filter.curves('b', [0, 40], [128, 128], [128, 128], [255, 215]);
+    
+    this.filter.gaussianBlur();
+  });
+  
+  this.curves('r', [20, 0], [128, 158], [128, 128], [235, 255]);
+  this.curves('g', [20, 0], [128, 128], [128, 128], [235, 255]);
+  this.curves('b', [20, 0], [128, 108], [128, 128], [235, 255]);
+  
+  this.vignette(250, 20);
+};
+
 }(Caman));
