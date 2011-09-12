5dabc131065461288958d3f902e3b7a160934db2
meltingice <meltingice8917@gmail.com>
2011-09-12 09:53:42 -0400

    Added new posterize plugin

--- dist/caman.full.js
+++ dist/caman.full.js
@@ -3276,6 +3276,29 @@ Caman.manip.emboss = function () {
 };
 
 }(Caman));
+if (!Caman && typeof exports == "object") {
+  var Caman = {manip:{}};
+  exports.plugins = Caman.manip;
+}
+
+(function (Caman) {
+
+//adjust must be [2...255]
+// works best with smaller input values
+Caman.manip.posterize = function (adjust) {
+  var numOfAreas = 256 / adjust;
+  var numOfValues = 255 / (adjust - 1);
+  
+  return this.process( [numOfAreas, numOfValues], function posterize (adjust, rgba) {
+    rgba.r = Math.floor(Math.floor( rgba.r / adjust[0] ) * adjust[1]);
+    rgba.g = Math.floor(Math.floor( rgba.g / adjust[0] ) * adjust[1]);
+    rgba.b = Math.floor(Math.floor( rgba.b / adjust[0] ) * adjust[1]);
+
+    return rgba;
+  });
+};
+
+}(Caman));
 /*global Caman: true, exports: true */
 
 /*
