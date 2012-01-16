2abc0cce88b9ae0fd5c609bf26549f090f1c6445
meltingice <meltingice8917@gmail.com>
2012-01-16 01:30:44 -0500

    Added edges plugin

--- dist/caman.full.js
+++ dist/caman.full.js
@@ -2106,4 +2106,16 @@
     });
   })();
 
+  Caman.Filter.register("edgeEnhance", function() {
+    return this.processKernel("Edge Enhance", [0, 0, 0, -1, 1, 0, 0, 0, 0]);
+  });
+
+  Caman.Filter.register("edgeDetect", function() {
+    return this.processKernel("Edge Detect", [-1, -1, -1, -1, 8, -1, -1, -1, -1]);
+  });
+
+  Caman.Filter.register("emboss", function() {
+    return this.processKernel("Emboss", [-2, -1, 0, -1, 1, 1, 0, 1, 2]);
+  });
+
 }).call(this);
